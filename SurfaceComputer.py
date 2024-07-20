9c0171cab76422febac1c53dbba02b43988e4842
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 21:02:19 +0300

    test changed

--- SurfaceComputer.py
+++ SurfaceComputer.py
@@ -2,34 +2,37 @@ import time
 # Import mavutil
 from pymavlink import mavutil
 
-def set_servo_pwm(servo_n, microseconds):
-    """ Sets AUX 'servo_n' output PWM pulse-width.
-
-    Uses https://mavlink.io/en/messages/common.html#MAV_CMD_DO_SET_SERVO
-
-    'servo_n' is the AUX port to set (assumes port is configured as a servo).
-        Valid values are 1-3 in a normal BlueROV2 setup, but can go up to 8
-        depending on Pixhawk type and firmware.
-    'microseconds' is the PWM pulse-width to set the output to. Commonly
-        between 1100 and 1900 microseconds.
-
+def wait_conn():
     """
-    # master.set_servo(servo_n+8, microseconds) or:
-    master.mav.command_long_send(
-        master.target_system, master.target_component,
-        mavutil.mavlink.MAV_CMD_DO_SET_SERVO,
-        0,            # first transmission of this command
-        servo_n + 8,  # servo instance, offset by 8 MAIN outputs
-        microseconds, # PWM pulse-width
-        0,0,0,0,0     # unused parameters
-    )
-
-
+    Sends a ping to stabilish the UDP communication and awaits for a response
+    """
+    msg = None
+    while not msg:
+        master.mav.ping_send(
+            int(time.time() * 1e6), # Unix time in microseconds
+            0, # Ping number
+            0, # Request ping of all systems
+            0 # Request ping of all components
+        )
+        msg = master.recv_match()
+        time.sleep(0.5)
 
 # Create the connection
-master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
-# command servo_1 to go from min to max in steps of 50us, over 2 seconds
+#  Companion is already configured to allow script connections under the port 9000
+# Note: The connection is done with 'udpout' and not 'udpin'.
+#  You can check in http:192.168.1.2:2770/mavproxy that the communication made for 9000
+#  uses a 'udp' (server) and not 'udpout' (client).
+master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
 
+# Send a ping to start connection and wait for any reply.
+#  This function is necessary when using 'udpout',
+#  as described before, 'udpout' connects to 'udpin',
+#  and needs to send something to allow 'udpin' to start
+#  sending data.
+wait_conn()
+
+# Arm
+# master.arducopter_arm() or:
 master.mav.command_long_send(
     master.target_system,
     master.target_component,
@@ -42,8 +45,14 @@ print("Waiting for the vehicle to arm")
 master.motors_armed_wait()
 print('Armed!')
 
-time.sleep(2)
+# Disarm
+# master.arducopter_disarm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    0, 0, 0, 0, 0, 0, 0)
 
-for us in range(1100, 1900, 50):
-    set_servo_pwm(1, us)
-    time.sleep(0.5)
+# wait until disarming confirmed
+master.motors_disarmed_wait()
