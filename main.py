f207c02a49088110997105bf18bc76832da3df79
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 13:42:29 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,91 @@
+import time
+
+# Import mavutil
+from pymavlink import mavutil
+
+
+# Imports for attitude
+
+
+def set_target_depth(depth):
+    """ Sets the target depth while in depth-hold mode.
+
+    Uses https://mavlink.io/en/messages/common.html#SET_POSITION_TARGET_GLOBAL_INT
+
+    'depth' is technically an altitude, so set as negative meters below the surface
+        -> set_target_depth(-1.5) # sets target to 1.5m below the water surface.
+
+    """
+    master.mav.set_position_target_global_int_send(
+        int(1e3 * (time.time() - boot_time)),  # ms since boot
+        master.target_system, master.target_component,
+        coordinate_frame=mavutil.mavlink.MAV_FRAME_GLOBAL_INT,
+        type_mask=(  # ignore everything except z position
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_X_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_Y_IGNORE |
+                # DON'T mavutil.mavlink.POSITION_TARGET_TYPEMASK_Z_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VX_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VY_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VZ_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AX_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AY_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AZ_IGNORE |
+                # DON'T mavutil.mavlink.POSITION_TARGET_TYPEMASK_FORCE_SET |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE
+        ), lat_int=0, lon_int=0, alt=depth,  # (x, y WGS84 frame pos - not used), z [m]
+        vx=0, vy=0, vz=0,  # velocities in NED frame [m/s] (not used)
+        afx=0, afy=0, afz=0, yaw=0, yaw_rate=0
+        # accelerations in NED frame [N], yaw, yaw_rate
+        #  (all not supported yet, ignored in GCS Mavlink)
+    )
+
+
+def set_target_velocity(vx, vy, vz):
+    """ Sets the target velocity while in depth-hold mode.
+
+    'vx', 'vy', and 'vz' are velocities in m/s in NED frame.
+
+    """
+    master.mav.set_position_target_local_ned_send(
+        int(1e3 * (time.time() - boot_time)),  # ms since boot
+        master.target_system, master.target_component,
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED,  # coordinate frame
+        0b0000111111000111,  # type mask (only velocities enabled)
+        0, 0, 0,  # x, y, z positions (not used)
+        vx, vy, vz,  # x, y, z velocity in m/s
+        0, 0, 0,  # x, y, z acceleration (not supported yet, ignored in GCS)
+        0, 0  # yaw, yaw rate (not used)
+    )
+
+
+# Create the connection
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+boot_time = time.time()
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
+
+# arm ArduSub autopilot and wait until confirmed
+master.arducopter_arm()
+master.motors_armed_wait()
+
+# set the desired operating mode
+DEPTH_HOLD = 'ALT_HOLD'
+DEPTH_HOLD_MODE = master.mode_mapping()[DEPTH_HOLD]
+while not master.wait_heartbeat().custom_mode == DEPTH_HOLD_MODE:
+    master.set_mode(DEPTH_HOLD)
+
+# set a depth target
+set_target_depth(-0.3)
+
+# move forward at 0.5 m/s for 2 seconds (total 1 meter)
+for _ in range(2):
+    set_target_velocity(0.5, 0, 0)  # move forward at 0.5 m/s
+    time.sleep(1)
+
+# stop the vehicle
+set_target_velocity(0, 0, 0)
+
+# clean up (disarm) at the end
+master.arducopter_disarm()
+master.motors_disarmed_wait()
