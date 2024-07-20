44aca172405be34ebf241384d0d283e19f182dc9
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 20:54:48 +0300

    test changed

--- SurfaceComputer.py
+++ SurfaceComputer.py
@@ -31,11 +31,28 @@ master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
 #  sending data.
 wait_conn()
 
-master.reboot_autopilot()
-# # Get some information !
-# while True:
-#     try:
-#         print(master.recv_match().to_dict())
-#     except:
-#         pass
-#     time.sleep(0.1)
+# Arm
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
+# Disarm
+# master.arducopter_disarm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    0, 0, 0, 0, 0, 0, 0)
+
+# wait until disarming confirmed
+master.motors_disarmed_wait()
