924b38369015b89d8de790f8eb9bb1b57d20aaf2
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 20:05:15 +0300

    test changed

--- Test.py
+++ Test.py
@@ -1,46 +1,13 @@
+"""
+Example of connecting to an autopilot via serial communication using pymavlink
+"""
+
+# Import mavutil
 from pymavlink import mavutil
 
-master = mavutil.mavlink_connection('/dev/ttyACM0', baud=57600)
+# Create the connection
+# Need to provide the serial port and baudrate
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 
-master.wait_heartbeat()
-print("Connected")
-
-def set_mode(mode):
-    # Mode listesi
-    mode_mapping = {
-        'STABILIZE': 0,
-        'GUIDED': 4,
-        'LOITER': 5,
-        'RTL': 6,
-        'AUTO': 10,
-    }
-
-    if mode not in mode_mapping:
-        print("sasd1")
-        return
-
-    master.set_mode(mode_mapping[mode])
-
-
-def arm_vehicle():
-    master.arducopter_arm()
-
-
-def disarm_vehicle():
-    master.arducopter_disarm()
-
-def takeoff(altitude):
-    arm_vehicle()
-
-    master.mav.command_long_send(
-        master.target_system,
-        master.target_component,
-        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-        0,
-        0, 0, 0, 0, 0, 0, altitude
-    )
-
-set_mode('STABILIZE')
-arm_vehicle()
-takeoff(10)
-disarm_vehicle()
+# Restart the ArduSub board !
+master.reboot_autopilot()
