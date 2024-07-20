504fea349e1d46d2716c58a0eb03f7fe6e104edf
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 21:07:15 +0300

    test changed

--- SurfaceComputer.py
+++ SurfaceComputer.py
@@ -1,6 +1,30 @@
 import time
 # Import mavutil
 from pymavlink import mavutil
+import time
+
+
+def set_servo_pwm(servo_n, microseconds):
+    """ Sets AUX 'servo_n' output PWM pulse-width.
+
+    Uses https://mavlink.io/en/messages/common.html#MAV_CMD_DO_SET_SERVO
+
+    'servo_n' is the AUX port to set (assumes port is configured as a servo).
+        Valid values are 1-3 in a normal BlueROV2 setup, but can go up to 8
+        depending on Pixhawk type and firmware.
+    'microseconds' is the PWM pulse-width to set the output to. Commonly
+        between 1100 and 1900 microseconds.
+
+    """
+    # master.set_servo(servo_n+8, microseconds) or:
+    master.mav.command_long_send(
+        master.target_system, master.target_component,
+        mavutil.mavlink.MAV_CMD_DO_SET_SERVO,
+        0,            # first transmission of this command
+        servo_n + 8,  # servo instance, offset by 8 MAIN outputs
+        microseconds, # PWM pulse-width
+        0,0,0,0,0     # unused parameters
+    )
 
 def wait_conn():
     """
@@ -22,8 +46,9 @@ def wait_conn():
 # Note: The connection is done with 'udpout' and not 'udpin'.
 #  You can check in http:192.168.1.2:2770/mavproxy that the communication made for 9000
 #  uses a 'udp' (server) and not 'udpout' (client).
-master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
+# master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
 
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 # Send a ping to start connection and wait for any reply.
 #  This function is necessary when using 'udpout',
 #  as described before, 'udpout' connects to 'udpin',
@@ -31,28 +56,10 @@ master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
 #  sending data.
 wait_conn()
 
-# Arm
-# master.arducopter_arm() or:
-master.mav.command_long_send(
-    master.target_system,
-    master.target_component,
-    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-    0,
-    1, 0, 0, 0, 0, 0, 0)
+# Wait a heartbeat before sending commands
 
-# wait until arming confirmed (can manually check with master.motors_armed())
-print("Waiting for the vehicle to arm")
-master.motors_armed_wait()
-print('Armed!')
+for us in range(1100, 1900, 50):
+    set_servo_pwm(1, us)
+    time.sleep(0.125)
 
-# Disarm
-# master.arducopter_disarm() or:
-master.mav.command_long_send(
-    master.target_system,
-    master.target_component,
-    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-    0,
-    0, 0, 0, 0, 0, 0, 0)
 
-# wait until disarming confirmed
-master.motors_disarmed_wait()
