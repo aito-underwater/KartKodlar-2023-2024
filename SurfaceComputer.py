10a26ca37f2949da4643884c8219ea85f40d5104
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 21:30:15 +0300

    test changed

--- SurfaceComputer.py
+++ SurfaceComputer.py
@@ -1,65 +1,22 @@
+import sys
 import time
+
 # Import mavutil
 from pymavlink import mavutil
-import time
-
-
-def set_servo_pwm(servo_n, microseconds):
-    """ Sets AUX 'servo_n' output PWM pulse-width.
-
-    Uses https://mavlink.io/en/messages/common.html#MAV_CMD_DO_SET_SERVO
-
-    'servo_n' is the AUX port to set (assumes port is configured as a servo).
-        Valid values are 1-3 in a normal BlueROV2 setup, but can go up to 8
-        depending on Pixhawk type and firmware.
-    'microseconds' is the PWM pulse-width to set the output to. Commonly
-        between 1100 and 1900 microseconds.
-
-    """
-    # master.set_servo(servo_n+8, microseconds) or:
-    master.mav.command_long_send(
-        master.target_system, master.target_component,
-        mavutil.mavlink.MAV_CMD_DO_SET_SERVO,
-        0,            # first transmission of this command
-        servo_n + 8,  # servo instance, offset by 8 MAIN outputs
-        microseconds, # PWM pulse-width
-        0,0,0,0,0     # unused parameters
-    )
-
-def wait_conn():
-    """
-    Sends a ping to stabilish the UDP communication and awaits for a response
-    """
-    msg = None
-    while not msg:
-        master.mav.ping_send(
-            int(time.time() * 1e6), # Unix time in microseconds
-            0, # Ping number
-            0, # Request ping of all systems
-            0 # Request ping of all components
-        )
-        msg = master.recv_match()
-        time.sleep(0.5)
 
 # Create the connection
-#  Companion is already configured to allow script connections under the port 9000
-# Note: The connection is done with 'udpout' and not 'udpin'.
-#  You can check in http:192.168.1.2:2770/mavproxy that the communication made for 9000
-#  uses a 'udp' (server) and not 'udpout' (client).
-# master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
-
 master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
-# Send a ping to start connection and wait for any reply.
-#  This function is necessary when using 'udpout',
-#  as described before, 'udpout' connects to 'udpin',
-#  and needs to send something to allow 'udpin' to start
-#  sending data.
-wait_conn()
-
 # Wait a heartbeat before sending commands
+# master.wait_heartbeat()
 
-for us in range(1100, 1900, 50):
-    set_servo_pwm(1, us)
-    time.sleep(0.125)
-
-
+# Request all parameters
+master.mav.param_request_list_send(master.target_system, master.target_component)
+while True:
+    time.sleep(0.01)
+    try:
+        message = master.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
+        print('name: %s\tvalue: %d' % (message['param_id'],
+                                       message['param_value']))
+    except Exception as error:
+        print(error)
+        sys.exit(0)
--- Test.py
+++ Test.py
@@ -1,13 +1,61 @@
-"""
-Example of connecting to an autopilot via serial communication using pymavlink
-"""
-
-# Import mavutil
+import time
 from pymavlink import mavutil
 
 # Create the connection
-# Need to provide the serial port and baudrate
-master = mavutil.mavlink_connection("/dev/ttyAMA0", baud=115200)
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
-# Restart the ArduSub board !   
-master.reboot_autopilot()
+# Wait for the first heartbeat
+while True:
+    msg = master.recv_match(type='HEARTBEAT', blocking=True)
+    if msg:
+        break
+
+# Request parameter value
+master.mav.param_request_read_send(
+    master.target_system, master.target_component,
+    b'SURFACE_DEPTH',
+    -1
+)
+
+# Print old parameter value
+while True:
+    message = master.recv_match(type='PARAM_VALUE', blocking=True)
+    if message and message.param_id == b'SURFACE_DEPTH':
+        print('name: %s\tvalue: %f' % (message.param_id.decode("utf-8"), message.param_value))
+        break
+
+time.sleep(1)
+
+# Set parameter value
+master.mav.param_set_send(
+    master.target_system, master.target_component,
+    b'SURFACE_DEPTH',
+    -12.0,  # New parameter value (float) to be set
+    mavutil.mavlink.MAV_PARAM_TYPE_REAL32
+)
+
+# Wait for ACK (PARAM_VALUE message confirming the change)
+while True:
+    message = master.recv_match(type='PARAM_VALUE', blocking=True)
+    if message and message.param_id == b'SURFACE_DEPTH':
+        print('name: %s\tvalue: %f' % (message.param_id.decode("utf-8"), message.param_value))
+        break
+
+time.sleep(1)
+
+# Request parameter value again to confirm the change
+master.mav.param_request_read_send(
+    master.target_system, master.target_component,
+    b'SURFACE_DEPTH',
+    -1
+)
+
+# Print new value in RAM
+while True:
+    message = master.recv_match(type='PARAM_VALUE', blocking=True)
+    if message and message.param_id == b'SURFACE_DEPTH':
+        print('name: %s\tvalue: %f' % (message.param_id.decode("utf-8"), message.param_value))
+        break
+
+# Close the connection
+master.close()
