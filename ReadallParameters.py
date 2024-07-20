c11039429863c4cbb57e1e927ff1b8b758590748
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 20:12:18 +0300

    test changed

--- ReadallParameters.py
+++ ReadallParameters.py
@@ -13,7 +13,7 @@ from pymavlink import mavutil
 
 
 # Create the connection
-master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+master =  mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 # Wait a heartbeat before sending commands
 print("Waiting heartbeat")
 master.wait_heartbeat()
