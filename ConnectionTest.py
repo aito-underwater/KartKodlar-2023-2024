a8a512e0eec7b0f5f76661754688fa79c2e70504
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-07-20 14:26:45 +0300

    Connection test added

---
+++ ConnectionTest.py
@@ -0,0 +1,8 @@
+from pymavlink import mavutil
+
+# Create the connection
+# Need to provide the serial port and baudrate
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+
+# Restart the ArduSub board !
+master.reboot_autopilot()
